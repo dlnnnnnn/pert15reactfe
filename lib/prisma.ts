2703a2779b7ea1a3cf6e1f89a1f2ebdbbb25828c
dlnnnnnn <rdlnelll.0@gmail.com>
2025-11-24 12:39:11 +0700

    minor changee

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,73 +1,36 @@
-// // import { PrismaClient } from "@prisma/client";
-
-// // const globalForPrisma =
-// //     globalThis as unknown as { prisma: PrismaClient };
-
-// // export const prisma =
-// //     globalForPrisma.prisma ?? new PrismaClient({
-// //         log: ["query"],
-// //     });
-
-// // if (process.env.NODE_ENV !== "production")
-// //     globalForPrisma.prisma = prisma;
-
-// import { PrismaClient } from '@prisma/client';
-// import path from 'path';
-
-// declare global {
-//   // eslint-disable-next-line no-var
-//   var cachedPrisma: PrismaClient;
-// }
-
-// // Workaround to find the db file in production
-// const filePath = path.join(process.cwd(), 'prisma/dev.db');
-// const config = {
-//   datasources: {
-//     db: {
-//       url: 'file:' + filePath,
-//     },
-//   },
-// };
-
-// let prisma: PrismaClient;
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient(config);
-// } else {
-//   if (!global.cachedPrisma) {
-//     global.cachedPrisma = new PrismaClient(config);
-//   }
-//   prisma = global.cachedPrisma;
-// }
-
-// export const db = prisma;
-
 import { PrismaClient } from '@prisma/client';
 import path from 'path';
 
-// Extend global type so Prisma doesn't get recreated in dev
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var cachedPrisma: PrismaClient;
 }
 
-// Absolute file path to SQLite database
-const dbPath = path.join(process.cwd(), 'prisma/dev.db');
+// Workaround to find the db file in production
+const filePath = path.join(process.cwd(), 'prisma/local.db');
 
-const prismaConfig = {
-  datasources: {
-    db: {
-      url: 'file:' + dbPath,
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: 'file:' + filePath,
+      },
     },
-  },
-};
-
-// Use cached client in development; new client in production
-const prisma =
-  process.env.NODE_ENV === 'production'
-    ? new PrismaClient(prismaConfig)
-    : global.prisma ?? new PrismaClient(prismaConfig);
-
-// Cache the client in dev
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+  });
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: 'file:' + filePath,
+        },
+      },
+      log: ['query'],
+    });
+  }
+  prisma = global.cachedPrisma;
+}
 
 export const db = prisma;
