3cc0b979e0f9ce19eddb30a76c5d82f879a4b810
dlnnnnnn <rdlnelll.0@gmail.com>
2025-11-24 12:45:36 +0700

    simplified

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,32 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-import path from 'path';
 
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient;
 }
 
-// Workaround to find the db file in production
-const filePath = path.join(process.cwd(), 'prisma/dev.db');
-
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: 'file:' + filePath,
-      },
-    },
-  });
+  prisma = new PrismaClient();
 } else {
   if (!global.cachedPrisma) {
     global.cachedPrisma = new PrismaClient({
-      datasources: {
-        db: {
-          url: 'file:' + filePath,
-        },
-      },
       log: ['query'],
     });
   }
